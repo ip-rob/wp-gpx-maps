ef4951d9675235434917b8cf6d35c1891857aa15
Rob <rob@Robs-iMac.lan>
2022-03-12 14:31:06 -0700

    Fix typo in js

--- js/WP-GPX-Maps.js
+++ js/WP-GPX-Maps.js
@@ -880,8 +880,8 @@ var WPGPXMAPS = {
 				photos.push({
 					'lat': pos[0],
 					'lng': pos[1],
-					'name': ngg_span_a.children[0].getAttribute( 'data-title' ),
-					'url': ngg_span_a.children[0].getAttribute( 'data-src' ),
+					'name': ngg_span_a.children[0].getAttribute( 'title' ),
+					'url': ngg_span_a.children[0].getAttribute( 'src' ),
 					'mwlimgid': ngg_span_a.children[0].getAttribute( 'data-mwl-img-id' ),
 					'thumbnail': ngg_span_a.children[0].getAttribute( 'src' )
 				});
